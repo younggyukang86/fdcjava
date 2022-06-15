70ffc005b78ff9fa4f933a7ba7664bb59ecb13fe
kangside21 <kangside21@gmail.com>
2022-06-16 00:42:23 +0900

    FIX - snappy unnest test 파일 쓰기 여러가지 방법 추가함.

--- snappyUnnestTest/src/main/java/Main.java
+++ snappyUnnestTest/src/main/java/Main.java
@@ -2,11 +2,14 @@ import org.apache.commons.io.IOUtils;
 import org.xerial.snappy.Snappy;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.sql.Timestamp;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -73,9 +76,15 @@ public final class Main {
     }
 
     private static long decompressTestDistributeFile(List<String> rowKeys, int threadNum, List<String> tableDatas) throws Exception {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        String folder = "c:\\test\\jsm_trace_test\\";
+        //String fileName = String.format("trace_data_write_%d.csv", System.currentTimeMillis());
+        String fileName = String.format("trace_data_write_%d.csv", System.nanoTime());
+        System.out.println(fileName);
+
+        // 1. 기존 샘플 소스
+        /*ByteArrayOutputStream baos = new ByteArrayOutputStream();
         long rowNum = 0;
-        for(String rowKey : rowKeys) {
+        for (String rowKey : rowKeys) {
             String rowData = getResultFromHbase(rowKey, tableDatas);
             List<String> colums = Arrays.asList(rowData.split("\\$"));
             String refValue = colums.get(colums.size() - 1).split("#")[1];
@@ -104,15 +113,123 @@ public final class Main {
                     rowNum++;
                 }
             }
-        }
+        }*/
 
-        File file = new File("c:\\test\\jsm_trace_test\\", String.format("trace_data_write_%d.csv", System.currentTimeMillis()));
+        // 기존 샘플 소스 io 전체 쓰기
+        /*File file = new File(folder, fileName);
         InputStream is = new ByteArrayInputStream(baos.toByteArray());
         try {
             IOUtils.copy(is, new FileOutputStream(file));
             is.close();
             System.out.println("[CSV FILE] create csv file!!!!");
         } catch(Exception e) {
+            throw new RuntimeException("CSV 파일 생성을 실패하였습니다." + e);
+        }*/
+
+        // 2. NIO 전체 쓰기
+        /*final int bufferMaxSize = 1024 * 1024;
+        try (FileChannel resultChannel = FileChannel.open(Paths.get(folder + fileName), StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+            ByteBuffer buffer = ByteBuffer.allocateDirect(bufferMaxSize);
+            buffer = ByteBuffer.wrap(baos.toByteArray());
+            resultChannel.write(buffer);
+            resultChannel.close();
+            System.out.println("[CSV FILE] create csv file!!!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("CSV 파일 생성을 실패하였습니다." + e);
+        }*/
+
+        // 3. for 문돌면서 NIO 조금씩 쓰기
+        /*long rowNum = 0;
+        final int bufferMaxSize = 1024 * 1024;
+        try (FileChannel resultChannel = FileChannel.open(Paths.get(folder + fileName), StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+            for (String rowKey : rowKeys) {
+                String rowData = getResultFromHbase(rowKey, tableDatas);
+                List<String> colums = Arrays.asList(rowData.split("\\$"));
+                String refValue = colums.get(colums.size() - 1).split("#")[1];
+                String rowKwyColumn = colums.get(0);
+
+                for (int i = 1; i < colums.size() - 1; i++) {
+                    String header = colums.get(i).split("#")[0];
+                    String qualifier = hbaseQualifierReplace(header.split("@")[0]);
+                    String columnFamily = header.split("@")[1];
+
+                    // uncompressed trace data
+                    String data = new String(Snappy.uncompress(decode(colums.get(i).split("#")[1])));
+                    // unnest trace data
+                    String[] datas = data.split(",");
+                    List<String> c3 = splitData(datas[2]);
+                    List<String> c4 = splitData(datas[3]);
+                    List<String> c5 = splitData(datas[4]);
+                    List<String> c6 = splitData(datas[5]);
+                    List<String> c7 = splitData(datas[6]);
+                    List<String> c8 = splitData(datas[7]);
+
+                    for (int j = 0; j < c3.size(); j++) {
+                        String traceData = String.format("%s,%s,%s,%s,%s,%s,%s,%s \n", datas[0], datas[1], c3.get(j), c4.get(j), c5.get(j), c6.get(j), c7.get(j), c8.get(j));
+                        String completeTraceData = String.format("%d,%s,%s,%s", rowNum, refValue, qualifier, traceData);
+
+                        ByteBuffer buffer = ByteBuffer.allocateDirect(bufferMaxSize);
+                        Charset charset = Charset.defaultCharset();
+                        buffer = charset.encode(completeTraceData);
+                        resultChannel.write(buffer);
+
+                        rowNum++;
+                    }
+                }
+            }
+
+            resultChannel.close();
+            System.out.println("[CSV FILE] create csv file!!!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("CSV 파일 생성을 실패하였습니다." + e);
+        }*/
+
+        // 4. for 문돌면서 IO 조금씩 쓰기
+
+        File file = new File(folder, fileName);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        long rowNum = 0;
+        try (FileOutputStream fos = new FileOutputStream(folder + fileName, true)) {
+
+            for (String rowKey : rowKeys) {
+                String rowData = getResultFromHbase(rowKey, tableDatas);
+                List<String> colums = Arrays.asList(rowData.split("\\$"));
+                String refValue = colums.get(colums.size() - 1).split("#")[1];
+                String rowKwyColumn = colums.get(0);
+
+                for (int i = 1; i < colums.size() - 1; i++) {
+                    String header = colums.get(i).split("#")[0];
+                    String qualifier = hbaseQualifierReplace(header.split("@")[0]);
+                    String columnFamily = header.split("@")[1];
+
+                    // uncompressed trace data
+                    String data = new String(Snappy.uncompress(decode(colums.get(i).split("#")[1])));
+                    // unnest trace data
+                    String[] datas = data.split(",");
+                    List<String> c3 = splitData(datas[2]);
+                    List<String> c4 = splitData(datas[3]);
+                    List<String> c5 = splitData(datas[4]);
+                    List<String> c6 = splitData(datas[5]);
+                    List<String> c7 = splitData(datas[6]);
+                    List<String> c8 = splitData(datas[7]);
+
+                    for (int j = 0; j < c3.size(); j++) {
+                        String traceData = String.format("%s,%s,%s,%s,%s,%s,%s,%s \n", datas[0], datas[1], c3.get(j), c4.get(j), c5.get(j), c6.get(j), c7.get(j), c8.get(j));
+                        String completeTraceData = String.format("%d,%s,%s,%s", rowNum, refValue, qualifier, traceData);
+                        fos.write(completeTraceData.getBytes(StandardCharsets.UTF_8));
+                        rowNum++;
+                    }
+                }
+            }
+
+            System.out.println("[CSV FILE] create csv file!!!!");
+        } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException("CSV 파일 생성을 실패하였습니다." + e);
         }
 
